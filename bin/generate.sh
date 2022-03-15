ff4e4f3de314d525941443e90f7b7a5513d7dc33
Przemek Szalko <p.szalko@gmail.com>
2022-03-15 08:47:25 +0100

    PHP 8.1

--- bin/generate.sh
+++ bin/generate.sh
@@ -4,9 +4,9 @@
 # Generates Dockerfiles for specified PHP versions
 #
 
-# PHP Versions that will be generated 
+# PHP Versions that will be generated
 #php_versions=( "8.0" "7.4" "7.2" "7.1" "7.0" "5.6")
-php_versions=( "8.0" "7.4" "7.2" )
+php_versions=( "8.1" "8.0" "7.4" "7.2" )
 
 # PHP variants that will be generated for each PHP version
 # final source image will be generated as follow: php:7.2-cli, php:7-2-apache and php:7.2-fpm
@@ -15,32 +15,33 @@ php_docker_suffix=( "cli" "apache" "fpm" )
 # Map of xdebug versions for PHP images
 # PHP_VERSION => XDEBUG_VERSION
 declare -A xdebug_versions
-xdebug_versions=( 
-    ["8.0"]="xdebug-3.0.4"
-	["7.4"]="xdebug-2.9.1"
-	["7.2"]="xdebug-2.6.0"
-	["7.1"]="xdebug-2.6.0"
-	["7.0"]="xdebug-2.6.0"
-	["5.6"]="xdebug-2.5.5"
+xdebug_versions=(
+    ["8.1"]="xdebug-3.1.3"
+    ["8.0"]="xdebug-3.1.3"
+    ["7.4"]="xdebug-2.9.1"
+    ["7.2"]="xdebug-2.6.0"
+    ["7.1"]="xdebug-2.6.0"
+    ["7.0"]="xdebug-2.6.0"
+    ["5.6"]="xdebug-2.5.5"
 )
 
 
 # Changes current path to project root
 script_path=$(dirname "$0")
-cd "${script_path}/../" || exit -1
+cd "${script_path}/../" || exit 1
 
 # Generates Dockerfiles for each PHP version and variant
-for php_version in "${php_versions[@]}"; do	
+for php_version in "${php_versions[@]}"; do
 	for php_suffix in "${php_docker_suffix[@]}"; do
 		target_dir="./build/${php_version}-${php_suffix}"
 		target_dockerfile="${target_dir}/Dockerfile"
-		
+
 		base_image="php:${php_version}-${php_suffix}"
 		xdebug_version="${xdebug_versions[${php_version}]}"
-		
+
 		mkdir -p "${target_dir}"
 		cp ./src/xdebug.ini "${target_dir}/xdebug.ini"
-		
+
 		# shellcheck disable=SC2002
 		cat ./src/Dockerfile \
 			| sed "s/#BASE_IMAGE#/${base_image}/g" \
