eb2c97be43d5813fff6e7add8ce5acc68966589b
Sam L <samlitowitz@users.noreply.github.com>
2025-11-26 14:16:24 -0500

    Do not generate files for PHP 7.2

--- bin/generate.sh
+++ bin/generate.sh
@@ -5,7 +5,7 @@
 #
 
 # PHP Versions that will be generated
-php_versions=( "8.3" "8.2" "8.1" "8.0" "7.4" "7.2" )
+php_versions=( "8.3" "8.2" "8.1" "8.0" "7.4" )
 
 # PHP variants that will be generated for each PHP version
 # final source image will be generated as follow: php:7.2-cli, php:7-2-apache and php:7.2-fpm
